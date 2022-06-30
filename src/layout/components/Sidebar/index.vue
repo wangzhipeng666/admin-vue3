b9774cc14628d9a3c6ccb5a29d30b529f04c4280
汪智鹏 <1848993253@qq.com>
2022-06-30 17:59:57 +0800

    临时menu

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="">Sidebar</div>
+  <div class="">
+    <h1>占位</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
+import SidebarMenu from './SidebarMenu'
 import {} from 'vue'
 </script>
-
-<style lang="scss" scoped></style>
