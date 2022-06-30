0debed4ea5105a1a1fc446476378256083643fae
汪智鹏 <1848993253@qq.com>
2022-06-30 11:02:48 +0800

    layout架构样式优化

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -4,6 +4,7 @@
     <sidebar
       id="guide-sidebar"
       class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
     />
     <div class="main-container">
       <div class="fixed-header">
@@ -21,7 +22,26 @@ import {} from 'vue'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import AppMain from './components/AppMain'
+import variables from '@/styles/variables.scss'
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
