54b37bee55807df7a518b38920fbc7a4b2633998
汪智鹏 <1848993253@qq.com>
2022-07-08 17:01:56 +0800

    静态面包屑组件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar">
     <hamburger class="hamburger-container" />
+    <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -34,6 +35,7 @@
 import {} from 'vue'
 import { useStore } from 'vuex'
 import Hamburger from '@/components/Hamburger'
+import Breadcrumb from '@/components/Breadcrumb'
 
 const store = useStore()
 const logout = () => {
@@ -48,7 +50,9 @@ const logout = () => {
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
-
+  .breadcrumb-container {
+    float: left;
+  }
   .right-menu {
     display: flex;
     align-items: center;
