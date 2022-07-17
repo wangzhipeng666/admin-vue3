f62ebe567c8e30cc3fa274e1ca28873292ed475b
wangzhipeng <zhipeng.wang@apicloud.com>
2022-07-17 12:20:34 +0800

    主题色变更功能

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,6 @@
-import variables from '@/styles/variables.scss'
+import { MAIN_COLOR } from '@/constant'
+import { getItem } from '@/utils/storage'
+import { generateColors } from '@/utils/theme'
 
 const getters = {
   token: state => state.user.token,
@@ -12,9 +14,15 @@ const getters = {
   /**
    * menu动态样式
    */
-  cssVar: state => variables,
+  cssVar: state => {
+    return {
+      ...state.theme.variables,
+      ...generateColors(getItem(MAIN_COLOR))
+    }
+  },
   sidebarOpened: state => state.app.sidebarOpened,
-  language: state => state.app.language
+  language: state => state.app.language,
+  mainColor: state => state.theme.mainColor
 }
 
 export default getters
